68a8f3454f5b9458673458de921a524cee99f97d
Josh <jmart790@fiu.edu>
2020-11-09 08:37:40 -0500

    implemented geo location in home with temp styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,71 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="home__title">My Dashboard Dot Com</h1>
+    <h2 class="home__sub-title">{{location.city || ''}}, {{location.region || ''}}</h2>
+    <WeatherWidget v-if="hasLocation" :location="location"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
+import WeatherWidget from "@/components/WeatherWidget";
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    WeatherWidget
+  },
+  data() {
+    return {
+      hasLocation: false,
+      location: {
+        city: '',
+        region: '',
+        latitude: '',
+        longitude: ''
+      },
+      isLoading: false,
+      error: null,
+    }
+  },
+  async created() {
+    const options = {
+      method: 'GET',
+      url: 'https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/',
+      params: {ip: '68.254.126.158'},
+      headers: {
+        'x-rapidapi-key': process.env.VUE_APP_RAPID_API_KEY,
+        'x-rapidapi-host': 'ip-geolocation-ipwhois-io.p.rapidapi.com'
+      }
+    };
+    this.isLoading = true;
+    await axios.request(options).then(response => {
+      this.location = response.data;
+      this.hasLocation = true;
+    }).catch(error => {
+      this.error = error
+    });
+    this.isLoading = false;
   }
 }
 </script>
+
+<style lang="scss">
+.home {
+  height: 100vh;
+  width: 100vw;
+  padding: 24px 20px;
+  background-color: $off-white;
+
+  &__title {
+    font-size: 40px;
+    text-align: center;
+    color: $primary;
+    text-shadow: 2px 2px 0 $grey-4;
+  }
+  &__sub-title {
+    text-align: center;
+  }
+}
+</style>
