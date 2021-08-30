b992a81f8684e3cd81e411cbfe1090787b5c4e39
Josh <jmart790@fiu.edu>
2021-08-30 17:56:21 -0400

    added google news api endpoints

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+const rapidApiKey = process.env.VUE_APP_RAPID_API_KEY;
 
 export default {
   /**
@@ -12,7 +13,7 @@ export default {
       url: 'https://weatherapi-com.p.rapidapi.com/timezone.json',
       params: {q: params },
       headers: {
-        'x-rapidapi-key': process.env.VUE_APP_RAPID_API_KEY,
+        'x-rapidapi-key': rapidApiKey,
         'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
       }
     };
@@ -29,9 +30,43 @@ export default {
       url: `https://dark-sky.p.rapidapi.com/${lat},${lon}`,
       params: { lang: "en", units: "auto" },
       headers: {
-        "x-rapidapi-key": process.env.VUE_APP_RAPID_API_KEY,
+        "x-rapidapi-key": rapidApiKey,
         "x-rapidapi-host": "dark-sky.p.rapidapi.com",
       },
+    };
+    return (await axios.request(options)).data;
+  },
+  /**
+   * 
+   * @param {*} payload is expecting city or state
+   * @returns an array of news objects
+   */
+  async getNewsByLocation(payload) {
+    const options = {
+      method: 'GET',
+      url: 'https://google-news1.p.rapidapi.com/geolocation',
+      params: {geo: payload, country: 'US', lang: 'en', limit: '50', media: 'true'},
+      headers: {
+        'x-rapidapi-host': 'google-news1.p.rapidapi.com',
+        'x-rapidapi-key': rapidApiKey
+      }
+    };
+    return (await axios.request(options)).data;
+  },
+  /**
+   * 
+   * @param {*} payload is a country or providence
+   * @returns an array of news objects
+   */
+  async getTopNewsHeadlines(payload) {
+    const options = {
+      method: 'GET',
+      url: 'https://google-news1.p.rapidapi.com/top-headlines',
+      params: {country: payload, lang: 'en', limit: '50', media: 'true'},
+      headers: {
+        'x-rapidapi-host': 'google-news1.p.rapidapi.com',
+        'x-rapidapi-key': rapidApiKey
+      }
     };
     return (await axios.request(options)).data;
   }
