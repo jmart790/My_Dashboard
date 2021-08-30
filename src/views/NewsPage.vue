d32d97d4e5800a34715451ea9f1a6784ab8a9a55
Josh <jmart790@fiu.edu>
2021-08-30 17:57:31 -0400

    creating basic page layout and testing response

--- src/views/NewsPage.vue
+++ src/views/NewsPage.vue
@@ -1,16 +1,39 @@
 <script>
+import { mapGetters, mapActions } from 'vuex';
+import PageHeader from '@/components/base/PageHeader';
+
 export default {
-  name: "NewsPage"
+  name: "NewsPage",
+  components: {
+    PageHeader
+  },
+   async mounted() {
+    if (this.location) {
+      await this.getLocalNews(this.location.name);
+    }
+  },
+  computed: {
+    ...mapGetters('location', ["location", "cityState"]),
+    ...mapGetters('news', ["news"]),
+  },
+  methods: {
+    ...mapActions('news', ["getLocalNews", "getStateNews", "getTopNewsHeadlines"]),
+  }
+
 }
 </script>
 
 <template>
-  <div>
-    News
+  <div class="news">
+    <PageHeader 
+      class="news__header"
+      title="News" 
+      :subtitle="cityState" 
+    />
   </div>
 </template>
 
 
-<style>
+<style lang="scss">
 
 </style>
