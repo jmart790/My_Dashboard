a18e07eac0352bedf59d9abbbf4c7e58afaa6540
Josh <jmart790@fiu.edu>
2021-08-31 12:05:43 -0400

    updated data, testing news resonse

--- src/views/NewsPage.vue
+++ src/views/NewsPage.vue
@@ -1,39 +1,44 @@
 <script>
-import { mapGetters, mapActions } from 'vuex';
-import PageHeader from '@/components/base/PageHeader';
+import { mapGetters, mapActions } from "vuex";
+import PageHeader from "@/components/base/PageHeader";
 
 export default {
   name: "NewsPage",
   components: {
-    PageHeader
+    PageHeader,
   },
-   async mounted() {
+  async mounted() {
     if (this.location) {
       await this.getLocalNews(this.location.name);
+      await this.getStateNews(this.location.region);
+      await this.getCountryNews(this.location.country);
     }
   },
   computed: {
-    ...mapGetters('location', ["location", "cityState"]),
-    ...mapGetters('news', ["news"]),
+    ...mapGetters("location", ["location", "cityState"]),
+    ...mapGetters("news", ["news", "isLoading"]),
   },
   methods: {
-    ...mapActions('news', ["getLocalNews", "getStateNews", "getTopNewsHeadlines"]),
-  }
-
-}
+    ...mapActions("news", ["getLocalNews", "getStateNews", "getCountryNews"]),
+  },
+};
 </script>
 
 <template>
   <div class="news">
-    <PageHeader 
-      class="news__header"
-      title="News" 
-      :subtitle="cityState" 
-    />
+    <PageHeader class="news__header" title="News" :subtitle="cityState" />
+    <article
+      v-for="(article, id) in news.country"
+      :key="`COUNTRY_NEWS_${id}`"
+      class="card"
+    >
+      <p>{{ article.title }}</p>
+      <p>{{ aticle.summary }}</p>
+      <p>{{ article.published_date }}</p>
+    </article>
   </div>
 </template>
 
 
 <style lang="scss">
-
 </style>
