0d443738f0e821b4323832770d35e151b1f9a10c
Josh <jmart790@fiu.edu>
2021-09-01 11:43:16 -0400

    imported country news and added light layout for large screens

--- src/views/NewsPage.vue
+++ src/views/NewsPage.vue
@@ -1,11 +1,13 @@
 <script>
 import { mapGetters, mapActions } from "vuex";
 import PageHeader from "@/components/base/PageHeader";
+import CountryNews from "@/components/news/CountryNews";
 
 export default {
   name: "NewsPage",
   components: {
     PageHeader,
+    CountryNews,
   },
   async mounted() {
     if (this.location) {
@@ -27,18 +29,42 @@ export default {
 <template>
   <div class="news">
     <PageHeader class="news__header" title="News" :subtitle="cityState" />
-    <article
-      v-for="(article, id) in news.country"
-      :key="`COUNTRY_NEWS_${id}`"
-      class="card"
-    >
-      <p>{{ article.title }}</p>
-      <p>{{ aticle.summary }}</p>
-      <p>{{ article.published_date }}</p>
-    </article>
+    <CountryNews
+      v-if="news.country.length"
+      :label="location.country"
+      :news="news.country"
+      class="news__country"
+    />
   </div>
 </template>
 
 
 <style lang="scss">
+.news {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-areas: "header" "weather";
+  gap: $gap-4;
+  padding-bottom: 80px;
+  @media screen and (min-width: $tablet) {
+    grid-template-columns: 1fr 1fr;
+    grid-template-areas:
+      "header header"
+      "country country";
+    gap: $gap-6;
+  }
+  @media screen and (min-width: $laptop) {
+    grid-template-columns: 1fr 1fr 450px;
+    grid-template-areas:
+      "header header country"
+      ".  .  country";
+  }
+  &__header {
+    grid-area: header;
+    margin-bottom: $gap-3;
+  }
+  &__country {
+    grid-area: country;
+  }
+}
 </style>
