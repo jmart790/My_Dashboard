40956c580c9dd23f36372e9cf85150522a357b4b
Josh <jmart790@fiu.edu>
2021-09-13 12:00:24 -0400

    created simple error card

---
+++ src/components/base/ErrorCard.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: "ErrorCard",
+  props: {
+    message: {
+      type: String,
+      required: true
+    },
+    category: {
+      type: String,
+      required: true,
+      validator: category => {
+        const validCategories = ["NEWS", "WEATHER", "FOOD"];
+        return validCategories.includes(category);
+      }
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="card">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+
+<style>
+</style>
