4e7c6f9c57d0ec5f4db960b4aeb394ef3f7af8e6
Josh <jmart790@fiu.edu>
2021-09-15 08:55:43 -0400

    filtering articles before mutatation and handling error dynamically

--- src/store/modules/news.js
+++ src/store/modules/news.js
@@ -6,9 +6,27 @@ const getDefaultState = () => ({
     state: [],
     country: [],
   },
+  error: {
+    local: false,
+    state: false,
+    country: false,
+  },
   isLoading: false,
-  error: null,
 });
+
+function filterArticles(articles) {
+  if (articles.length < 1) return [];
+  const articlesWithContent = articles.filter(
+    (article) => article.media && article.clean_url && article.title && article.summary
+  );
+  const seen = new Set();
+  const articlesWithoutDupes = articlesWithContent.filter((article) => {
+    const duplicate = seen.has(article.title);
+    seen.add(article.title);
+    return !duplicate;
+  });
+  return articlesWithoutDupes;
+}
 
 export default {
   namespaced: true,
@@ -26,8 +44,8 @@ export default {
     SET_IS_LOADING(state, value) {
       state.isLoading = value;
     },
-    SET_ERROR(state, value) {
-      state.error = value;
+    SET_ERROR(state, { key, error }) {
+      state[key] = error;
     },
     RESET_DATA(state) {
       Object.assign(state, getDefaultState());
@@ -43,27 +61,30 @@ export default {
       commit('SET_IS_LOADING', true);
       await apiClient
         .getNewsByLocation(payload)
-        .then((data) => commit('SET_LOCAL_NEWS', data.articles))
-        .catch((error) => commit('SET_ERROR', error));
+        .then((data) => commit('SET_LOCAL_NEWS', filterArticles(data.articles)))
+        .catch(({ response }) => commit('SET_ERROR', { response, errorType: 'local' }));
       await new Promise((resolve) => setTimeout(resolve, 2000));
+      commit('SET_ERROR', { key: 'local', error: false });
       commit('SET_IS_LOADING', false);
     },
     async getStateNews({ commit }, payload) {
       commit('SET_IS_LOADING', true);
       await apiClient
         .getNewsByLocation(payload)
-        .then((data) => commit('SET_STATE_NEWS', data.articles))
-        .catch((error) => commit('SET_ERROR', error));
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+        .then((data) => commit('SET_STATE_NEWS', filterArticles(data.articles)))
+        .catch(({ response }) => commit('SET_ERROR', { response, errorType: 'state' }));
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      commit('SET_ERROR', { key: 'state', error: false });
       commit('SET_IS_LOADING', false);
     },
     async getCountryNews({ commit }, payload) {
       commit('SET_IS_LOADING', true);
       await apiClient
         .getNewsByLocation(payload)
-        .then((data) => commit('SET_COUNTRY_NEWS', data.articles))
-        .catch((error) => commit('SET_ERROR', error));
+        .then((data) => commit('SET_COUNTRY_NEWS', filterArticles(data.articles)))
+        .catch(({ response }) => commit('SET_ERROR', { response, errorType: 'country' }));
       await new Promise((resolve) => setTimeout(resolve, 2000));
+      commit('SET_ERROR', { key: 'country', error: false });
       commit('SET_IS_LOADING', false);
     },
     resetState: {
