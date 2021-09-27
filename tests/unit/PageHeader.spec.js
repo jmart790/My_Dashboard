f036be9525da39f4dd85926d061b4364646d34d4
Josh <jmart790@fiu.edu>
2021-09-27 11:22:51 -0400

    adding test

--- tests/unit/PageHeader.spec.js
+++ tests/unit/PageHeader.spec.js
@@ -1,23 +1,37 @@
-import { shallowMount } from '@vue/test-utils'
-import PageHeader from '@/components/base/PageHeader.vue'
+import { shallowMount } from '@vue/test-utils';
+import PageHeader from '@/components/base/PageHeader.vue';
 
 describe('PageHeader.vue', () => {
+  const title = 'New Title';
+  const wrapper = shallowMount(PageHeader, {
+    propsData: { title },
+  });
+  const input = wrapper.find('input');
+  const button = wrapper.find('button');
+
+
 
   it('renders props.title when passed', () => {
-    const title = 'New Title';
-    const wrapper = shallowMount(PageHeader, {
-      propsData: { title }
-    });
     expect(wrapper.text()).toMatch(title);
   });
 
   it('renders props.subtitle when passed', () => {
-    const title = 'New Title';
     const subtitle = 'New Sub Title';
-    const wrapper = shallowMount(PageHeader, {
-      propsData: { title, subtitle }
+    const anotherWrapper = shallowMount(PageHeader, {
+      propsData: { title, subtitle },
     });
-    expect(wrapper.text()).toMatch(subtitle);
+    expect(anotherWrapper.text()).toMatch(subtitle);
   });
 
-})
+  it('sets the value', async () => {
+    await input.setValue('33137');
+    expect(input.element.value).toBe('33137');
+  });
+
+  it('zipcode submits', async () => {
+    await input.setValue('33137');
+    await button.trigger('click');
+    // assert some action has been performed, like an emitted event.
+    expect(wrapper.emitted()).toHaveProperty('submit')
+  });
+});
