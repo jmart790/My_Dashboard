e42d7ebbe3b703baa63a0cfbf418812590e1ca84
Josh <jmart790@fiu.edu>
2021-09-28 19:43:00 -0400

    created restaurant component

---
+++ src/components/food/Restaurant.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "restaurant",
+  prop: {
+    restaurant: {
+      type: Object,
+      Required: true
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="restaurant">
+    test
+  </div>
+</template>
+
+
+<style lang="scss">
+.restaurant {
+}
+</style>
