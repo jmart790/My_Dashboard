fabc25aad177430570040532f7e3c4e844e2edd0
Josh <jmart790@fiu.edu>
2021-09-29 12:54:49 -0400

    renamed component and created spacing between children

---
+++ src/components/food/RestaurantList.vue
@@ -0,0 +1,37 @@
+<script>
+import Restaurant from "@/components/food/Restaurant";
+
+export default {
+  name: "RestaurantList",
+  components: { Restaurant },
+  props: {
+    restaurants: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="restaurants card">
+    <Restaurant
+      class="restaurants__restaurant"
+      v-for="(restaurant, index) in restaurants"
+      :key="`RESTAURANT-${index}`"
+      :restaurant="restaurant"
+    />
+  </div>
+</template>
+
+<style lang="scss">
+.restaurants {
+  height: 100%;
+  padding-right: $gap-4;
+  border-radius: $round-2;
+  overflow: auto;
+  &__restaurant {
+    margin-bottom: $gap-8;
+  }
+}
+</style>
